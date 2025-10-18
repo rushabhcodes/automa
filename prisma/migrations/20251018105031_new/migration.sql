de133586ced1f9c7239e76487c0915e374436684
rushabhcodes <mail@rushabh.dev>
2025-10-18 17:04:33 +0530

    feat(auth): integrate better-auth for user authentication and session management
    - Added better-auth package for improved authentication handling.
    - Updated User model in Prisma schema to include email verification and image fields.
    - Introduced Session and Account models to manage user sessions and accounts.
    - Implemented requireAuth and requireUnauth utilities for route protection.
    - Created login and signup pages with forms for user authentication.
    - Integrated login and signup forms with better-auth client for handling user sign-in and registration.
    - Updated TRPC procedures to fetch users based on authenticated session.
    - Added Toaster component for displaying success and error messages.

---
+++ prisma/migrations/20251018105031_new/migration.sql
@@ -0,0 +1,39 @@
+/*
+  Warnings:
+
+  - The primary key for the `user` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - Made the column `name` on table `user` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."account" DROP CONSTRAINT "account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."session" DROP CONSTRAINT "session_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "account" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "session" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "user" DROP CONSTRAINT "user_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "name" SET NOT NULL,
+ADD CONSTRAINT "user_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "user_id_seq";
+
+-- DropTable
+DROP TABLE "public"."Post";
+
+-- AddForeignKey
+ALTER TABLE "session" ADD CONSTRAINT "session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "account" ADD CONSTRAINT "account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ src/features/auth/components/signup-form.tsx
@@ -0,0 +1,188 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardTitle,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardDescription,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { authClient } from "@/lib/auth-client";
+
+const registerSchema = z.object({
+    name: z.string().min(2, "Name must be at least 2 characters"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z.string()
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirmPassword"],
+});
+
+type RegisterFormData = z.infer<typeof registerSchema>;
+
+export const RegisterForm = () => {
+    const router = useRouter();
+
+    const form = useForm<RegisterFormData>({
+        resolver: zodResolver(registerSchema),
+        defaultValues: {
+            name: "",
+            email: "",
+            password: "",
+            confirmPassword: "",
+        },
+    });
+
+    const onSubmit = async (data: RegisterFormData) => {
+
+        authClient.signUp.email(
+            {
+                name: data.email,
+                email: data.email,
+                password: data.password,
+                callbackURL: "/",
+            },
+            {
+                onSuccess: () => {
+                    toast.success("Registered successfully!");
+                    router.push("/");
+                },
+                onError: (ctx) => {
+                    toast.error(`Registration failed: ${ctx.error.message}`);
+                },
+            }
+        )
+    };
+
+    const isPending = form.formState.isSubmitting;
+
+    return (
+        <div
+            className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+
+            <Card>
+                <CardHeader>
+                    <CardTitle className="text-2xl text-center mb-4">
+                        Get Started with Automa
+                    </CardTitle>
+                    <CardDescription className="text-sm text-center">
+                        Please enter your email and password to register.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="space-y-4"
+                        >
+                            <Button
+                                variant="outline" className="w-full" disabled={isPending} type="button"
+                            >Continue with Github</Button>
+                            <Button
+                                variant="outline" className="w-full" disabled={isPending} type="button"
+                            >Continue with Google</Button>
+
+
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Enter your name"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+
+                            <FormField
+                                control={form.control}
+                                name="email"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Email</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                placeholder="Enter your email"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="password"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Password</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                type="password"
+                                                placeholder="Enter your password"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="confirmPassword"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Confirm Password</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                type="password"
+                                                placeholder="Confirm your password"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <Button type="submit" disabled={isPending}>
+                                {isPending ? "Logging in..." : "Sign Up"}
+                            </Button>
+                        </form>
+                    </Form>
+                </CardContent>
+                <CardFooter>
+                    <p className="text-sm text-center">
+                        Already have an account?{" "}
+                        <Link href="/login" className="text-blue-500">
+                            Log In
+                        </Link>
+                    </p>
+                </CardFooter>
+            </Card>
+        </div>
+    );
+};
