b8f494684484fe6679408337caeb78414cffd923
rushabhcodes <mail@rushabh.dev>
2025-10-19 14:55:20 +0530

    feat: enhance workflows management with new components, hooks, and server-side logic; add user association to workflows

---
+++ src/features/workflows/components/workflows.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { EntityContainer, EntityHeader } from "@/components/entity-components";
+import { useCreateWorkflow, useSuspenseWorkflows } from "../hooks/use-workflows";
+import { useUpgradeModal } from "@/hooks/use-upgrade-modal";
+import { useRouter } from "next/navigation";
+
+export const WorkflowsList = () => {
+    const workflows = useSuspenseWorkflows();
+
+    return (
+        <div className=" flex flex-col gap-4 justify-center items-center">
+            {workflows.data?.map((workflow) => (
+                <div key={workflow.id}>{workflow.name}</div>
+            ))}
+            hello world
+        </div>
+    );
+};
+
+export const WorkflowsHeader = ({ disabled }: { disabled?: boolean }) => {
+    const router = useRouter();
+    const createWorkflow = useCreateWorkflow();
+    const { handleError, modal } = useUpgradeModal();
+
+    const handleCreate = () => {
+        createWorkflow.mutate(undefined, {
+            onSuccess: (data) => {
+                router.push(`/workflows/${data.id}`);
+            },
+            onError: (error) => {
+                handleError(error);
+            }
+        });
+    };
+    return (
+        <>
+            {modal}
+            <EntityHeader
+                title="Workflows"
+                description="Create and manage your workflows"
+                newButtonLabel="Create Workflow"
+                disabled={disabled}
+                isCreating={createWorkflow.isPending}
+                onNew={handleCreate}
+            />
+        </>
+    )
+};
+
+export const WorkflowsContainer = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <EntityContainer
+            header={<WorkflowsHeader />}
+            search={<></>}
+            pagination={<></>}
+        >
+            {children}
+        </EntityContainer>
+    );
+}
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,43 +1,8 @@
-import { generateText } from 'ai';
-import { google } from '@ai-sdk/google';
-import { inngest } from '@/inngest/client';
-import { createTRPCRouter, protectedProcedure } from '../init';
-import prisma from '@/lib/db';
+import { createTRPCRouter } from '../init';
+import { workflowsRouter } from '@/features/workflows/servers/routers';
 
 export const appRouter = createTRPCRouter({
-
-    testAi: protectedProcedure
-        .mutation(async ({ ctx }) => {
-            await inngest.send({
-                name: 'automa/execute-ai',
-                data: {
-                    language: 'typescript',
-                }
-            });
-
-            return 'AI generation requested';
-        }),
-
-    getWorkflows: protectedProcedure
-        .query(({ ctx }) => {
-            return prisma.workflow.findMany(
-            );
-        }),
-
-    createWorkflow: protectedProcedure
-        .mutation(async ({ ctx }) => {
-            await inngest.send({
-                name: 'automa/hello.world',
-                data: {
-                    email: ctx.auth.user.email,
-                },
-            })
-
-            return {
-                success: true,
-                message: 'Job queued successfully',
-            }
-        }),
+    workflows: workflowsRouter
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
